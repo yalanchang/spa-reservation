840afaa30bc68e87ac008585007fd75b66d512ac
張雅嵐 <zhangyalan@zhangyalandeMacBook-Pro.local>
2026-03-11 23:49:32 +0800

    add svg showpassword

--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -63,19 +63,17 @@
                             placeholder="請再次輸入密碼" />
                         <button type="button" @click="showConfirmPassword = !showConfirmPassword"
                             class="absolute inset-y-0 right-3 flex items-center text-gray-400 hover:text-gray-600">
-                            <!-- 眼睛開 -->
                             <svg v-if="showConfirmPassword" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a9.97 9.97 0 012.163-3.592M6.47 6.47A9.97 9.97 0 0112 5c4.477 0 8.268 2.943 9.542 7a9.97 9.97 0 01-1.357 2.524M6.47 6.47L3 3m3.47 3.47l11.06 11.06M3 3l18 18" />
+                            </svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                            </svg>
-                            <!-- 眼睛關 -->
-                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a9.97 9.97 0 012.163-3.592M6.47 6.47A9.97 9.97 0 0112 5c4.477 0 8.268 2.943 9.542 7a9.97 9.97 0 01-1.357 2.524M6.47 6.47L3 3m3.47 3.47l11.06 11.06M3 3l18 18" />
                             </svg>
                         </button>
                     </div>
